17f7b1dfaa21046220f5ef186e1d3ece5c113b2f
Leo <xinlichao2016@gmail.com>
2020-09-29 11:43:32 +0800

    Add commons-pool2 Dependency

--- changeVersion.sh
+++ changeVersion.sh
@@ -1,6 +1,6 @@
-mvn versions:set -DnewVersion=1.0.0
-# mvn versions:set -DnewVersion=1.0.0-SNAPSHOT
-# mvn versions:set -DnewVersion=1.0.0-RELEASE
+# mvn versions:set -DnewVersion=1.0.1
+mvn versions:set -DnewVersion=1.0.1-SNAPSHOT
+# mvn versions:set -DnewVersion=1.0.1-RELEASE
 # mvn -N versions:update-child-modules
 # mvn versions:revert
 mvn versions:commit
--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,9 @@
-mvn clean deploy -Dmaven.test.skip=true -P release -projects lock4j-core,lock4j-spring-boot-autoconfigure,lock4j-spring-boot-starter
 
+# release all
+mvn clean deploy -Dmaven.test.skip=true -P release
+
+# exclude projects
+# mvn clean deploy -Dmaven.test.skip=true -P release -pl "!lock4j-core,!lock4j-spring-boot-autoconfigure,!lock4j-spring-boot-starter"
+
+# release projects
+# mvn clean deploy -Dmaven.test.skip=true -P release -projects lock4j-core,lock4j-spring-boot-autoconfigure,lock4j-spring-boot-starter
